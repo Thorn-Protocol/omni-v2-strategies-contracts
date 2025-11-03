eefb08129f30164a30cae7d7a5307ec1c3133376
phuongnguyenlaaithe <phuongnguyen2522003@gmail.com>
2025-11-03 10:36:35 +0700

    change new offchain strategy address

--- scripts/changeAgent.ts
+++ scripts/changeAgent.ts
@@ -2,12 +2,13 @@ import { ethers } from "hardhat";
 import { OffChainStrategy__factory } from "../typechain-types";
 
 async function main() {
-  let vault = OffChainStrategy__factory.connect("0xD2a9dB8f22707166e82EdF89534340237780eDA3", ethers.provider);
+  let vault = OffChainStrategy__factory.connect("0x00aa576bfa5f75BC6C651e8Cb587dD78b287040A", ethers.provider);
 
   let governance = await vault.governance();
   console.log(" governance", governance);
   let wallet = new ethers.Wallet(process.env.DEPLOYER!, ethers.provider);
-  let tx = await vault.connect(wallet).changeAgent("0x662Db28dEB8F649521262fB58C69D87b830e8276");
+  const nonce = await ethers.provider.getTransactionCount(wallet.address, "latest");
+  let tx = await vault.connect(wallet).changeAgent("0x662Db28dEB8F649521262fB58C69D87b830e8276", { nonce });
   let receipt = await tx.wait();
   console.log(" receipt", receipt?.hash);
 }
--- scripts/test.ts
+++ scripts/test.ts
@@ -2,7 +2,7 @@ import { ethers } from "hardhat";
 import { OffChainStrategy__factory } from "../typechain-types";
 
 async function main() {
-  let vault = OffChainStrategy__factory.connect("0xE049bdA7B0Ebb039C18671E13A65b4cfd6c8FaE5", ethers.provider);
+  let vault = OffChainStrategy__factory.connect("0x00aa576bfa5f75BC6C651e8Cb587dD78b287040A", ethers.provider);
 
   let agent = await vault.agent();
   console.log(" agent", agent);
